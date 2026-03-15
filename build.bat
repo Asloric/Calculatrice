548dd6abd9941d173c25177181d921c7723f558e
Asloric <43351133+Asloric@users.noreply.github.com>
2026-03-15 18:11:13 +0100

    made icon

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-"C:/Program Files/Python38/python.exe" -m PyInstaller --noconfirm --clean --windowed --onefile --name Calculatrice calculatrice_new.py
+"C:/Program Files/Python38/python.exe" -m PyInstaller --noconfirm --clean --windowed --onefile --icon .\calculatrice.ico --name Calculatrice calculatrice_new.py
